9e940819bc3ae33eeafa7ef040f5a641f14836b0
jaygustawo <jgusmunoz@gmail.com>
2021-04-14 00:14:02 -0500

    Added Exercises 1-1 1-2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react'
+const Header = (props) => {
+  return (
+    <>
+      <p>{props.course.name}</p>
+    </>
+  )
+}
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </>
+  )
+}
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack Application Development',
+    parts: [
+      {
+        name:'Fundamentals of React',
+        exercises:10
+      },
+      {
+        name:'Using  props to pass data',
+        exercises:7
+      },
+      {
+        name:'State of a component',
+        exercises:14
+      },
+
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course}/>
+      <Total  course={course}/>
+    </div>
+  )
+}
+
+export default App
